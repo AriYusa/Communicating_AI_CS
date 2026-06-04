cb0eafde988d98ddc9d060ab146a9e18c210184a
Beatrice-Beacco <beatrice.beacco@gmail.com>
2026-06-04 14:48:22 +0200

    feat: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import { projects } from "./data/projects";
 
 export default function App() {
   return (
-    <div style={{ padding: 40, color: "white", background: "#0f172a", minHeight: "100vh" }}>
+    <div
+      style={{
+        padding: 40,
+        color: "white",
+        background: "#0f172a",
+        minHeight: "100vh",
+      }}
+    >
+      <Header />
       <h1>AI&CS Projects</h1>
 
       <div style={{ display: "grid", gap: 20 }}>
@@ -16,13 +26,14 @@ export default function App() {
               background: "#1e293b",
               borderRadius: 12,
               color: "white",
-              textDecoration: "none"
+              textDecoration: "none",
             }}
           >
             {p.name}
           </a>
         ))}
       </div>
+      <Footer />
     </div>
   );
 }
